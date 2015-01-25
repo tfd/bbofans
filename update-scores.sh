2d1cf176aa1b2ee24edd2adde3b2517398b8ba0c
Ronald van Uffelen <ronald.vanuffelen@gmail.com>
2015-01-26 00:31:26 +0100

    Corrected update-scores.sh

--- update-scores.sh
+++ update-scores.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$( dirname "$0" )"
-wget http://127.0.0.1:3000//tournaments/update -q -O update-tournaments.json
+wget http://127.0.0.1:3000/tournaments/update -q -O update-tournaments.json
 echo "[$(date)] update score" >> update-tournaments.log
 cat update-tournaments.json >> update-tournaments.log
 echo " " >> update-tournaments.log
