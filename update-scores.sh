b595a31651b4290a2df2497130184e41a01c2978
Ronald van Uffelen <ronald.vanuffelen@gmail.com>
2015-01-26 00:23:06 +0100

    Updated scripts for tournaments and blacklist update

--- update-scores.sh
+++ update-scores.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$( dirname "$0" )"
-wget http://127.0.0.1:3000/update -q -O update.json
-echo "[$(date)] update score" >> update.log
-cat update.json >> update.log
-echo " " >> update.log
+wget http://127.0.0.1:3000//tournaments/update -q -O update-tournaments.json
+echo "[$(date)] update score" >> update-tournaments.log
+cat update-tournaments.json >> update-tournaments.log
+echo " " >> update-tournaments.log
