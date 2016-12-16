85e66567f917405a0f8f2531086dda5713247e8f
Ronald van Uffelen <ronald.vanuffelen@gmail.com>
2016-12-16 09:01:42 +0100

    Suspended registration

--- server/src/controllers/register.js
+++ server/src/controllers/register.js
@@ -19,6 +19,8 @@ module.exports = function (config) {
 
   return {
     register: function (req, res) {
+      return res.status(403).json({error: 'Temporarily unavailable'});
+      /*
       var member = req.body;
       config.servers.reCaptcha.checkDirect(req, member['g-recaptcha-response'], function (data) {
         if (data.success === false) {
@@ -76,6 +78,7 @@ module.exports = function (config) {
           }
         });
       });
+      */
     },
 
     getRegistrant: function (req, res) {
